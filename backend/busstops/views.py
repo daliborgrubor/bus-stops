f8adc6e1b6bb5e192f5b638d45a575d81204195a
Dado <grubor.it@gmail.com>
2023-03-03 10:32:51 +0100

    backend updated

--- backend/busstops/views.py
+++ backend/busstops/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import StopPointSerializer
+from .models import StopPoint
 
 # Create your views here.
+
+class StopPointView(viewsets.ModelViewSet):
+    serializer_class = StopPointSerializer
+    queryset = StopPoint.objects.all()
