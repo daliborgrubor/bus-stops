e917707fd05404695f74600878ca7b3da0f2b1da
Dado <grubor.it@gmail.com>
2023-03-07 15:05:57 +0100

    index, main, json modified

--- frontend/map/main.js
+++ frontend/map/main.js
@@ -1,17 +1,91 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import {Feature, Map, Overlay, View} from 'ol/index.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Point} from 'ol/geom.js';
+
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {useGeographic} from 'ol/proj.js';
+
+useGeographic();
+
+const place = [17.1910, 44.7722];
+
+const point = new Point(place);
 
 const map = new Map({
   target: 'map',
+  view: new View({
+    center: place,
+    zoom: 15,
+  }),
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        features: [new Feature(point)],
+      }),
+      style: {
+        'circle-radius': 9,
+        'circle-fill-color': 'red',
+      },
+    }),
   ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+});
+
+const element = document.getElementById('popup');
+
+const popup = new Overlay({
+  element: element,
+  stopEvent: false,
+});
+map.addOverlay(popup);
+
+function formatCoordinate(coordinate) {
+  return `
+    <table>
+      <tbody>
+        <tr><th>lon</th><td>${coordinate[0].toFixed(4)}</td></tr>
+        <tr><th>lat</th><td>${coordinate[1].toFixed(4)}</td></tr>
+      </tbody>
+    </table>`;
+}
+
+const info = document.getElementById('info');
+map.on('moveend', function () {
+  const view = map.getView();
+  const center = view.getCenter();
+  info.innerHTML = formatCoordinate(center);
+});
+
+let popover;
+map.on('click', function (event) {
+  if (popover) {
+    popover.dispose();
+    popover = undefined;
+  }
+  const feature = map.getFeaturesAtPixel(event.pixel)[0];
+  if (!feature) {
+    return;
+  }
+  const coordinate = feature.getGeometry().getCoordinates();
+  popup.setPosition([
+    coordinate[0] + Math.round(event.coordinate[0] / 360) * 360,
+    coordinate[1],
+  ]);
+
+  popover = new bootstrap.Popover(element, {
+    container: element.parentElement,
+    content: formatCoordinate(coordinate),
+    html: true,
+    offset: [0, 20],
+    placement: 'top',
+    sanitize: false,
+  });
+  popover.show();
+});
+
+map.on('pointermove', function (event) {
+  const type = map.hasFeatureAtPixel(event.pixel) ? 'pointer' : 'inherit';
+  map.getViewport().style.cursor = type;
 });
