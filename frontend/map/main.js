2d77538945338d5ed5c862c0d2e333d9e94db539
Dado <grubor.it@gmail.com>
2023-03-08 12:32:11 +0100

    draw on map

--- frontend/map/main.js
+++ frontend/map/main.js
@@ -1,91 +1,52 @@
-import {Feature, Map, Overlay, View} from 'ol/index.js';
+import Draw from 'ol/interaction/Draw.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
 import {OSM, Vector as VectorSource} from 'ol/source.js';
-import {Point} from 'ol/geom.js';
-
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
-import {useGeographic} from 'ol/proj.js';
 
-useGeographic();
+const raster = new TileLayer({
+  source: new OSM(),
+});
 
-const place = [17.1910, 44.7722];
+const source = new VectorSource({wrapX: false});
 
-const point = new Point(place);
+const vector = new VectorLayer({
+  source: source,
+});
 
 const map = new Map({
+  layers: [raster, vector],
   target: 'map',
   view: new View({
-    center: place,
-    zoom: 15,
+    center: [-11000000, 4600000],
+    zoom: 4,
   }),
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-    new VectorLayer({
-      source: new VectorSource({
-        features: [new Feature(point)],
-      }),
-      style: {
-        'circle-radius': 9,
-        'circle-fill-color': 'red',
-      },
-    }),
-  ],
 });
 
-const element = document.getElementById('popup');
+const typeSelect = document.getElementById('type');
 
-const popup = new Overlay({
-  element: element,
-  stopEvent: false,
-});
-map.addOverlay(popup);
-
-function formatCoordinate(coordinate) {
-  return `
-    <table>
-      <tbody>
-        <tr><th>lon</th><td>${coordinate[0].toFixed(4)}</td></tr>
-        <tr><th>lat</th><td>${coordinate[1].toFixed(4)}</td></tr>
-      </tbody>
-    </table>`;
+let draw; // global so we can remove it later
+function addInteraction() {
+  const value = typeSelect.value;
+  if (value !== 'None') {
+    draw = new Draw({
+      source: source,
+      type: typeSelect.value,
+    });
+    map.addInteraction(draw);
+  }
 }
 
-const info = document.getElementById('info');
-map.on('moveend', function () {
-  const view = map.getView();
-  const center = view.getCenter();
-  info.innerHTML = formatCoordinate(center);
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+document.getElementById('undo').addEventListener('click', function () {
+  draw.removeLastPoint();
 });
 
-let popover;
-map.on('click', function (event) {
-  if (popover) {
-    popover.dispose();
-    popover = undefined;
-  }
-  const feature = map.getFeaturesAtPixel(event.pixel)[0];
-  if (!feature) {
-    return;
-  }
-  const coordinate = feature.getGeometry().getCoordinates();
-  popup.setPosition([
-    coordinate[0] + Math.round(event.coordinate[0] / 360) * 360,
-    coordinate[1],
-  ]);
-
-  popover = new bootstrap.Popover(element, {
-    container: element.parentElement,
-    content: formatCoordinate(coordinate),
-    html: true,
-    offset: [0, 20],
-    placement: 'top',
-    sanitize: false,
-  });
-  popover.show();
-});
-
-map.on('pointermove', function (event) {
-  const type = map.hasFeatureAtPixel(event.pixel) ? 'pointer' : 'inherit';
-  map.getViewport().style.cursor = type;
-});
+addInteraction();
